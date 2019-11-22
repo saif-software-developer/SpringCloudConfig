9c2c66d3d18a05250181382b26e0245705694321
Saif Masadeh <saif.masadeh@coxautoinc.com>
2019-11-21 21:30:47 -0600

    added the cloud-config-server and 2 clients

---
+++ employee-service/src/test/java/com/saif/main/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saif.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
